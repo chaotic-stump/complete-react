51fc2f605ef189c6db7a27dd657cca747cfb0448
Sean Parker <seanparker@PECOS-MAC06.local>
2018-02-06 13:54:46 -0500

    setting routes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import IndecisionApp from "./components/IndecisionApp";
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
+import { BrowserRouter, Route } from "react-router-dom";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById("app")
+const ExpenseDashboardPage = () => (
+  <div>
+    This is my dashboard
+  </div>
 );
+
+const AddExpensePage = () => (
+  <div>
+    This is my Add Expense Component.
+  </div>
+);
+
+const EditExpensePage = () => (
+  <div>
+    This is my Edit Expense Page.
+  </div>
+)
+
+const HelpPage = () => (
+  <div>
+    This is my Help Page.
+  </div>
+)
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("app"));
